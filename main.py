8c04999b5d09fdca9dee43ebf97166288d0eaf43
jac726 <53344954+jac726@users.noreply.github.com>
2019-07-29 16:52:49 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+types1 = ["American", "Barbecue", "Chinese", "French", "Hamburger", "Indian", "Italian", "Japanese", "Mexican", "Pizza", "Seafood", "Steak", "Sushi", "Thai"]
+
+types2 = ["Movies", "Music", "Performances", "Parks", "Museums", "Shopping Malls", "Game Rooms", "Sports"]
+
+restaurant_type = random.choice(types1)
+
+entertainment_type = random.choice(types2)
+
+print restaurant_type
+
+print entertainment_type
