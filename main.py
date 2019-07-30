f10ceddb3217e4e76e16a354207ebee9376a6ddb
jac726 <53344954+jac726@users.noreply.github.com>
2019-07-30 18:09:54 -0400

    Static fix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import jinja2
 import os
 import random
 
-
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
