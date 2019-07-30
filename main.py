7dcf7a0ebadb2fafb18699659e56c25c7acacd86
jac726 <53344954+jac726@users.noreply.github.com>
2019-07-30 16:48:26 -0400

    Template Vars

--- main.py
+++ main.py
@@ -19,14 +19,16 @@ class Restaurant(webapp2.RequestHandler):
         template = the_jinja_env.get_template('templates/restaurant.html')
         types1 = ["American", "Barbecue", "Chinese", "French", "Hamburger", "Indian", "Italian", "Japanese", "Mexican", "Pizza", "Seafood", "Steak", "Sushi", "Thai"]
         restaurant_type = random.choice(types1)
-        self.response.write(template.render())  # the response
+        template_vars = {"type" : restaurant_type}
+        self.response.write(template.render(template_vars))  # the response
 
 class Entertainment(webapp2.RequestHandler):
     def get(self):
         template = the_jinja_env.get_template('templates/entertainment.html')
         types2 = ["Movies", "Music", "Performances", "Parks", "Museums", "Shopping Malls", "Game Rooms", "Sports"]
         entertainment_type = random.choice(types2)
-        self.response.write(template.render())  # the response
+        template_vars = {"type" : entertainment_type}
+        self.response.write(template.render(template_vars))  # the response
 
 class Locations(webapp2.RequestHandler):
     def get(self):
