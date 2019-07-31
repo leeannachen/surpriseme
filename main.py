c61c17ed740f9c5c6f4709656bc687980b2b9304
jac726 <53344954+jac726@users.noreply.github.com>
2019-07-31 10:26:55 -0400

    New Functions

--- main.py
+++ main.py
@@ -8,36 +8,41 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def Restaurant():
+    types1 = ["American", "Barbecue", "Chinese", "French", "Hamburger", "Indian", "Italian", "Japanese", "Mexican", "Pizza", "Seafood", "Steak", "Sushi", "Thai"]
+    restaurant_type = random.choice(types1)
+    return restaurant_type
+
+def Location():
+    types2 = ["Movies", "Music", "Performances", "Parks", "Museums", "Shopping Malls", "Game Rooms", "Sports"]
+    entertainment_type = random.choice(types2)
+    template_vars = {"type" : entertainment_type}
+    return entertainment_type
+
 class WelcomePage(webapp2.RequestHandler):
     def get(self):
         template = the_jinja_env.get_template('main.html')
         self.response.write(template.render())  # the response
 
-class Restaurant(webapp2.RequestHandler):
+class Result(webapp2.RequestHandler):
     def get(self):
-        template = the_jinja_env.get_template('restaurant.html')
-        types1 = ["American", "Barbecue", "Chinese", "French", "Hamburger", "Indian", "Italian", "Japanese", "Mexican", "Pizza", "Seafood", "Steak", "Sushi", "Thai"]
-        restaurant_type = random.choice(types1)
-        template_vars = {"type" : restaurant_type}
-        self.response.write(template.render(template_vars))  # the response
+        template = the_jinja_env.get_template('result.html')
 
-class Entertainment(webapp2.RequestHandler):
-    def get(self):
-        template = the_jinja_env.get_template('entertainment.html')
-        types2 = ["Movies", "Music", "Performances", "Parks", "Museums", "Shopping Malls", "Game Rooms", "Sports"]
-        entertainment_type = random.choice(types2)
-        template_vars = {"type" : entertainment_type}
-        self.response.write(template.render(template_vars))  # the response
-
-class Locations(webapp2.RequestHandler):
-    def get(self):
-        template = the_jinja_env.get_template('locations.html')
-        self.response.write(template.render())  # the response
+# class Restaurant(webapp2.RequestHandler):
+#     def get(self):
+#         template = the_jinja_env.get_template('restaurant.html')
+#         template_vars = {"type" : restaurant_type}
+#         self.response.write(template.render(template_vars))  # the response
+#
+# class Entertainment(webapp2.RequestHandler):
+#     def get(self):
+#         template = the_jinja_env.get_template('entertainment.html')
+#         self.response.write(template.render(template_vars))  # the response
+#
 
 app = webapp2.WSGIApplication([
     ('/', WelcomePage),
     ('/restaurant', Restaurant),
     ('/entertainment', Entertainment),
-    ('/locations', Locations)
 
 ], debug=True)
